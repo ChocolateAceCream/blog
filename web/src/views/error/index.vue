040adfb3ef05ff22edd56c28734ffeb6e166993a
ChocolateAceCream <nuodi@hotmail.com>
2023-02-28 03:01:35 +0800

    FIX: redo 404 page

--- web/src/views/error/index.vue
+++ web/src/views/error/index.vue
@@ -1,45 +1,76 @@
 <template>
-  <div>
-    <div class="big">
-      <div class="inner">
-        <img src="../../assets/notFound.png">
-        <p>页面被神秘力量吸走了（如果您是开源版请联系我们修复）</p>
-        <p style="font-size:18px;line-height:40px;">常见问题为当前此角色无当前路由，如果确定要使用本路由，请到角色管理进行分配</p>
-        <p>↓</p>
-        <img src="../../assets/qm.png" class="leftPic">
+  <div class="wrapper">
+    <div class="inner">
+      <div class="text-wrapper">
+        <p style="text-align: left;font-weight:bold;font-size:35px;">404</p>
+
+        <p style="text-align: left;">Current page is missing</p>
+        <el-button
+          type="primary"
+          @click="returnHome"
+        >Home</el-button>
+        <div>
+          <el-link
+            style=" font-weight:bold;font-size:15px;"
+            type="primary"
+            @click="goBack"
+          >or, return to previous page</el-link>
+        </div>
+
       </div>
+      <img src="../../assets/images/404.png">
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Error'
+import router from '@/router'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Error',
+  setup(props, ctx) {
+    const returnHome = () => {
+      router.push({name: 'home'})
+    }
+    const goBack = () => {
+      router.go(-1)
+    }
+    return {
+      returnHome,
+      goBack
+    }
+  }
 
-}
+})
+
+
 </script>
 
 <style lang="scss">
-    .big{
-        width: 100%;
-        height: calc(100vh - 220px);
-        background-color: rgb(244,244,244);
-        position: relative;
-    }
-    .inner{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-    .inner p {
-        text-align: center;
-        font-size: 24px;
-    }
-    .inner .leftPic{
-        width: 60px;
-        height: 60px;
-        margin-left: 44%;
-        margin-top: 20px;
-    }
+.wrapper{
+  height: 100%;
+  width: 100%;
+  display:flex;
+  .text-wrapper{
+    width:300px;
+
+  }
+  img {
+    width:22  0px;
+  }
+}
+.inner{
+    margin:auto;
+    display: flex;
+}
+.inner p {
+    text-align: center;
+    font-size: 24px;
+}
+.inner .leftPic{
+    width: 60px;
+    height: 60px;
+    margin-left: 44%;
+    margin-top: 20px;
+}
 </style>
