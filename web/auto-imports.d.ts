816a718b442216ebf102a7bf206e76463020494f
ChocolateAceCream <nuodi@hotmail.com>
2023-10-21 09:11:57 +0800

    FIX: clean up index.html

--- web/auto-imports.d.ts
+++ web/auto-imports.d.ts
@@ -2,6 +2,14 @@
 export {}
 declare global {
   const EffectScope: typeof import('vue')['EffectScope']
+  const ElForm: typeof import('element-plus/es')['ElForm']
+  const ElFormItem: typeof import('element-plus/es')['ElFormItem']
+  const ElInput: typeof import('element-plus/es')['ElInput']
+  const ElLoading: typeof import('element-plus/es')['ElLoading']
+  const ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+  const ElMessage: typeof import('element-plus/es')['ElMessage']
+  const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
+  const ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
   const computed: typeof import('vue')['computed']
   const createApp: typeof import('vue')['createApp']
   const customRef: typeof import('vue')['customRef']
